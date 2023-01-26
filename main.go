94782bfe847aea5099db899a7aeea44806303dad
Isaac S <4isaac6@proton.me>
2023-01-25 16:48:25 -0800

    Add Basic Route
    This commit adds the first route to the project at the API root

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
     "log"
     "net/http"
+    "encoding/json"
 
     "github.com/gorilla/mux"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(struct { Message string } {
+        Message: "Welcome to the Hello World API",
+    })
+}
+
 func main() {
 	router := mux.NewRouter()
 
-    log.Fatal(http.ListenAndServe(":8080", router))
+    router.HandleFunc("/api", home).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
