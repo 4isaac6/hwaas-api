8d8d485613749b76d220200b658a873f8f58b6b5
Isaac S <4isaac6@proton.me>
2023-01-25 16:48:24 -0800

    Add Mux Router
    This commit adds a Mux router, but without any routes

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-    "fmt"
+    "log"
+    "net/http"
+
+    "github.com/gorilla/mux"
 )
 
 func main() {
-    fmt.Println("Hello World!")
+	router := mux.NewRouter()
+
+    log.Fatal(http.ListenAndServe(":8080", router))
 }
