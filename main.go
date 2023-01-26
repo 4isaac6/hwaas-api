1d29ce6f3419fd6ff3515a47f2388ad921228bed
Isaac S <4isaac6@proton.me>
2023-01-25 16:48:25 -0800

    `languages` Route
    This commit adds a route to get the languages in the `leachim6/hello-world` repository; it parses the list in the README, avoiding multiple API calls for each of the directories used to organize the "Hello World" files

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+    "fmt"
     "log"
+    "time"
+    "regexp"
+    "context"
     "net/http"
     "encoding/json"
 
+    "github.com/spf13/viper"
     "github.com/gorilla/mux"
+    "github.com/google/go-github/v49/github"
 )
+
+type Language struct {
+    Name            string      `"json:name"`
+    Extension       string      `"json:extension"`
+}
+
+type LanguagesResponse struct {
+    Languages       []*Language `"json:languages"`
+    RequestedAt     time.Time   `"json:requested_at"`
+}
+
+var ctx context.Context
 
 func home(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
@@ -15,10 +33,74 @@ func home(w http.ResponseWriter, r *http.Request) {
     })
 }
 
+func getLanguages(w http.ResponseWriter, r *http.Request) {
+    var languages   []*Language
+
+    client := github.NewClient(nil)
+
+    // Get the README object.
+    readme, _, err := client.Repositories.GetReadme(
+        ctx,
+        viper.GetString("repository.user"),
+        viper.GetString("repository.name"),
+        nil,
+    )
+
+    if err != nil {
+        fmt.Printf("Error: %v\n", err)
+        return
+    }
+
+    // Get the README contents.
+    s, _ := readme.GetContent()
+
+    reExtension := regexp.MustCompile("(\\..+)")
+    reLanguage  := regexp.MustCompile("\\* \\[(.+)\\]\\(.+\\)\n")
+
+    // Find list of languages: "* [Language Name](lang.ext)"
+    for _, m := range reLanguage.FindAllStringSubmatch(s, -1) {
+        // Not all languages have file extensions; search separately.
+        ext := reExtension.FindString(m[0])
+
+        languages = append(languages, &Language{
+            Name: m[1],
+            Extension: ext,
+        })
+    }
+
+    res := &LanguagesResponse{
+        Languages: languages,
+        RequestedAt: time.Now(),
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(res)
+}
+
 func main() {
 	router := mux.NewRouter()
+    ctx = context.Background()
+
+    // Load `env` configuration.
+    viper.SetConfigName("env")
+    viper.AddConfigPath("config")
+    if err := viper.ReadInConfig(); err != nil {
+    	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+    		// Config file not found
+	        fmt.Printf("Error: %v\n", err)
+    	} else {
+    		// Config file was found but another error was produced
+            fmt.Printf("Error: %v\n", err)
+    	}
+        return
+    }
 
     router.HandleFunc("/api", home).Methods("GET")
+    router.HandleFunc("/api/languages", getLanguages).Methods("GET")
+    // router.HandleFunc("/api/languages/{language}", getLanguage).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(
+        fmt.Sprintf(":%d", viper.GetInt("server.port")),
+        router,
+    ))
 }
